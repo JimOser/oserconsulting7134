44b176688e794866b37ba2bb7e99c3cf052ab9c8
Jim Oser <oserj@oserconsulting.com>
2024-06-16 22:26:54 +0000

    helpers and views files to get bootstrap correct for my website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+# https://human-se.github.io/rails-demos-n-deets-2020/demo-bootstrap-navbar/  
+def active_class(link_path)
+    current_page?(link_path) ? 'active' : ""
+  end
+  # https://www.mitchcraver.com/2022/04/01/markdown-in-ruby-on-rails-with-redcarpet/
+  def markdown(text)
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks, :underline, :highlight,
+               :no_images, :filter_html, :safe_links_only, :prettify, :no_styles]
+    Markdown.new(text, *options).to_html.html_safe
+  end
+
 end
